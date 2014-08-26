7b28f43e713c53624fd41b03d21b44a7ad63c5bb
Maarten Decat <maarten.decat@gmail.com>
2014-08-26 17:27:54 +0200

    changed import of stapl.core.subject to explicit declaration to avoid working on a single object

--- src/main/scala/stapl/core/BasicPolicy.scala
+++ src/main/scala/stapl/core/BasicPolicy.scala
@@ -8,11 +8,16 @@ package stapl.core
  * Example: see staple.core.examples.PolicyFromReadMe 
  */
 trait BasicPolicy extends DelayedInit {
-
-  val subject = stapl.core.subject // FIXME do we work on the single subject object here? we need a local copy of some sort
-  val resource = stapl.core.resource
-  val action = stapl.core.action
-  val env = stapl.core.environment
+  
+  // repeat the definitions from stapl.core so that we do not work 
+  // on a single subject/resource/action/evironment object
+  val subject = new SubjectAttributeContainer
+  subject.id = SimpleAttribute(String) 
+  val resource = new ResourceAttributeContainer
+  resource.id = SimpleAttribute(String)
+  val action = new ActionAttributeContainer
+  action.id = SimpleAttribute(String) 
+  val environment = new EnvironmentAttributeContainer
   
   override def delayedInit(body: => Unit): Unit = {
     val code = (() => body)
