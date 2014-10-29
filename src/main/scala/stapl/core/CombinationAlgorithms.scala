d0f0704f6e512d678fba3050ff57d6d6659f0392
Maarten Decat <maarten.decat@gmail.com>
2014-10-29 17:06:08 +0100

    separated combination algorithm implementation from keyword

--- src/main/scala/stapl/core/CombinationAlgorithms.scala
+++ src/main/scala/stapl/core/CombinationAlgorithms.scala
@@ -22,87 +22,120 @@ package stapl.core
 import scala.annotation.tailrec
 import stapl.core.pdp.EvaluationCtx
 
-trait CombinationAlgorithm {
+/**
+ * *************************
+ * WRAPPERS
+ * 
+ * We separate the keywords from the implementation so that these can be overridden
+ * at runtime by passing them to the evaluation context.
+ */
+sealed trait CombinationAlgorithm {
+
+  def combine(policies: List[AbstractPolicy], ctx: EvaluationCtx): Result =
+    ctx.getCombinationAlgorithmImplementation(this).combine(policies, ctx)
+}
+
+case object PermitOverrides extends CombinationAlgorithm
+case object DenyOverrides extends CombinationAlgorithm
+case object FirstApplicable extends CombinationAlgorithm
+
+/**
+ * **********************
+ * IMPLEMENTATIONS
+ */
+trait CombinationAlgorithmImplementation {
 
   def combine(policies: List[AbstractPolicy], ctx: EvaluationCtx): Result
 }
 
-object PermitOverrides extends CombinationAlgorithm {
-
-  override def combine(policies: List[AbstractPolicy], ctx: EvaluationCtx): Result = {
-
-    var tmpResult = Result(NotApplicable)
-
-    for (policy <- policies) {
-      val Result(decision, obligationActions, _) = policy.evaluate(ctx)
-      // If a subpolicy returns Permit: return this result with its obligations,
-      //	do not evaluate the rest for other obligations. 
-      // TODO is this correct?
-      // If all subpolicies return Deny: combine all their obligations and return 
-      // 	them with Deny
-      // If all subpolicies return NotApplicable: return NotApplicable without obligations
-      // See XACML2 specs, Section 7.14
-      decision match {
-        case Permit => 
-          // we only need one Permit and only retain those obligations => jump out of the loop
-          return Result(decision, obligationActions) 
-        case Deny =>
-          // retain all obligations of previous Denies
-          tmpResult = Result(Deny, tmpResult.obligationActions ::: obligationActions)
-        case NotApplicable => // nothing to do
-      }
-    }
-    // if we got here: return the tmpResult with Deny or NotApplicable
-    tmpResult
-  }
+trait CombinationAlgorithmImplementationBundle {
+  def PermitOverrides: CombinationAlgorithmImplementation
+  def DenyOverrides: CombinationAlgorithmImplementation
+  def FirstApplicable: CombinationAlgorithmImplementation
 }
 
-object DenyOverrides extends CombinationAlgorithm {
+/**
+ * The bundle of simple implementations: sequential evaluation.
+ */
+object SimpleCombinationAlgorithmImplementationBundle extends CombinationAlgorithmImplementationBundle {
+  
+  object PermitOverrides extends CombinationAlgorithmImplementation {
 
-  override def combine(policies: List[AbstractPolicy], ctx: EvaluationCtx): Result = {
+    override def combine(policies: List[AbstractPolicy], ctx: EvaluationCtx): Result = {
 
-    var tmpResult = Result(NotApplicable)
+      var tmpResult = Result(NotApplicable)
 
-    for (policy <- policies) {
-      val Result(decision, obligationActions, _) = policy.evaluate(ctx)
-      // If a subpolicy returns Deny: return this result with its obligations,
-      //	do not evaluate the rest for other obligations. 
-      // TODO is this correct?
-      // If all subpolicies return Permit: combine all their obligations and return 
-      // 	them with Permit
-      // If all subpolicies return NotApplicable: return NotApplicable without obligations
-      // See XACML2 specs, Section 7.14
-      decision match {
-        case Deny => 
-          // we only need one Deny and only retain those obligations => jump out of the loop
-          return Result(decision, obligationActions) 
-        case Permit =>
-          // retain all obligations of previous Permits
-          tmpResult = Result(Permit, tmpResult.obligationActions ::: obligationActions)
-        case NotApplicable => // nothing to do
+      for (policy <- policies) {
+        val Result(decision, obligationActions, _) = policy.evaluate(ctx)
+        // If a subpolicy returns Permit: return this result with its obligations,
+        //	do not evaluate the rest for other obligations. 
+        // TODO is this correct?
+        // If all subpolicies return Deny: combine all their obligations and return 
+        // 	them with Deny
+        // If all subpolicies return NotApplicable: return NotApplicable without obligations
+        // See XACML2 specs, Section 7.14
+        decision match {
+          case Permit =>
+            // we only need one Permit and only retain those obligations => jump out of the loop
+            return Result(decision, obligationActions)
+          case Deny =>
+            // retain all obligations of previous Denies
+            tmpResult = Result(Deny, tmpResult.obligationActions ::: obligationActions)
+          case NotApplicable => // nothing to do
+        }
       }
+      // if we got here: return the tmpResult with Deny or NotApplicable
+      tmpResult
     }
-    // if we got here: return the tmpResult with Permit or NotApplicable
-    tmpResult
   }
-}
 
-object FirstApplicable extends CombinationAlgorithm {
+  object DenyOverrides extends CombinationAlgorithmImplementation {
 
-  override def combine(policies: List[AbstractPolicy], ctx: EvaluationCtx): Result = {
+    override def combine(policies: List[AbstractPolicy], ctx: EvaluationCtx): Result = {
 
-    var tmpResult = Result(NotApplicable)
+      var tmpResult = Result(NotApplicable)
 
-    for (policy <- policies) {
-      val Result(decision, obligationActions, _) = policy.evaluate(ctx)
-      decision match {
-        case Permit | Deny => 
-          // we only need one Deny or Permit and only retain those obligations => jump out of the loop
-          return Result(decision, obligationActions) 
-        case NotApplicable => // nothing to do
+      for (policy <- policies) {
+        val Result(decision, obligationActions, _) = policy.evaluate(ctx)
+        // If a subpolicy returns Deny: return this result with its obligations,
+        //	do not evaluate the rest for other obligations. 
+        // TODO is this correct?
+        // If all subpolicies return Permit: combine all their obligations and return 
+        // 	them with Permit
+        // If all subpolicies return NotApplicable: return NotApplicable without obligations
+        // See XACML2 specs, Section 7.14
+        decision match {
+          case Deny =>
+            // we only need one Deny and only retain those obligations => jump out of the loop
+            return Result(decision, obligationActions)
+          case Permit =>
+            // retain all obligations of previous Permits
+            tmpResult = Result(Permit, tmpResult.obligationActions ::: obligationActions)
+          case NotApplicable => // nothing to do
+        }
       }
+      // if we got here: return the tmpResult with Permit or NotApplicable
+      tmpResult
     }
-    // if we got here: return the tmpResult with Permit or NotApplicable
-    tmpResult
+  }
+
+  object FirstApplicable extends CombinationAlgorithmImplementation {
+
+    override def combine(policies: List[AbstractPolicy], ctx: EvaluationCtx): Result = {
+
+      var tmpResult = Result(NotApplicable)
+
+      for (policy <- policies) {
+        val Result(decision, obligationActions, _) = policy.evaluate(ctx)
+        decision match {
+          case Permit | Deny =>
+            // we only need one Deny or Permit and only retain those obligations => jump out of the loop
+            return Result(decision, obligationActions)
+          case NotApplicable => // nothing to do
+        }
+      }
+      // if we got here: return the tmpResult with Permit or NotApplicable
+      tmpResult
+    }
   }
 }
--- src/main/scala/stapl/core/pdp/EvaluationCtx.scala
+++ src/main/scala/stapl/core/pdp/EvaluationCtx.scala
@@ -24,27 +24,35 @@ import stapl.core.ConcreteValue
 import stapl.core.Attribute
 import stapl.core.AttributeContainerType
 import stapl.core.AttributeNotFoundException
+import stapl.core.PermitOverrides
+import stapl.core.DenyOverrides
+import stapl.core.FirstApplicable
+import stapl.core.CombinationAlgorithm
+import stapl.core.CombinationAlgorithmImplementation
+import stapl.core.CombinationAlgorithmImplementationBundle
+import stapl.core.SimpleCombinationAlgorithmImplementationBundle
 
 /**
  * The base class of the context for evaluating a policy. This context
- * represents all information for that policy evaluation, such as the 
- * id of the subject, the id of the resource, the id of the action and 
+ * represents all information for that policy evaluation, such as the
+ * id of the subject, the id of the resource, the id of the action and
  * a method to find attributes.
- * 
+ *
  * The method to find attributes is required in the evaluation context
  * because certain aspects such as an attribute cache are specific for
  * each individual evaluation context.
  */
 trait EvaluationCtx {
-  
+
   def evaluationId: Long
   def subjectId: String
   def resourceId: String
   def actionId: String
   def remoteEvaluator: RemoteEvaluator
-  def cachedAttributes: Map[Attribute,ConcreteValue]
+  def cachedAttributes: Map[Attribute, ConcreteValue]
   protected[core] def findAttribute(attribute: Attribute): ConcreteValue
-  
+  protected[core] def getCombinationAlgorithmImplementation(algo: CombinationAlgorithm): CombinationAlgorithmImplementation
+
   // TODO add type checking here
   //final def findAttribute(attribute: Attribute): ConcreteValue = 
 }
@@ -54,24 +62,25 @@ trait EvaluationCtx {
  * stores the subject id, the resource id, the action id and stores found
  * attribute values in a cache for this evaluation context.
  */
-class BasicEvaluationCtx(override val evaluationId: Long, request: RequestCtx, 
-    finder: AttributeFinder, override val remoteEvaluator: RemoteEvaluator) extends EvaluationCtx with Logging {
-  
-  override val subjectId: String = request.subjectId
-  
-  override val resourceId: String = request.resourceId
-  
-  override val actionId: String = request.actionId
-  
-  protected val attributeCache: scala.collection.mutable.Map[Attribute,ConcreteValue] = scala.collection.mutable.Map()
+class BasicEvaluationCtx(override val evaluationId: Long, request: RequestCtx,
+  finder: AttributeFinder, override val remoteEvaluator: RemoteEvaluator,
+  algos: CombinationAlgorithmImplementationBundle = SimpleCombinationAlgorithmImplementationBundle) extends EvaluationCtx with Logging {
 
-  override def cachedAttributes: Map[Attribute,ConcreteValue] = attributeCache.toMap
-  
+  override val subjectId: String = request.subjectId
+
+  override val resourceId: String = request.resourceId
+
+  override val actionId: String = request.actionId
+
+  protected val attributeCache: scala.collection.mutable.Map[Attribute, ConcreteValue] = scala.collection.mutable.Map()
+
+  override def cachedAttributes: Map[Attribute, ConcreteValue] = attributeCache.toMap
+
   // add all attributes given in the request to the attribute cache
-  for((attribute, value) <- request.allAttributes) {
+  for ((attribute, value) <- request.allAttributes) {
     attributeCache(attribute) = value
   }
-                            
+
   /**
    * Try to find the value of the given attribute. If the value is already
    * in the attribute cache, that value is returned. Otherwise, the attribute
@@ -85,13 +94,13 @@ class BasicEvaluationCtx(override val evaluationId: Long, request: RequestCtx,
         value
       }
       case None => { // Not in the cache
-        try{
+        try {
           val value: ConcreteValue = finder.find(this, attribute)
           attributeCache(attribute) = value // add to cache
           debug("FLOW: retrieved value of " + attribute + ": " + value + " and added to cache")
           value
         } catch {
-          case e: AttributeNotFoundException => 
+          case e: AttributeNotFoundException =>
             debug(s"Didn't find value of $attribute anywhere, exception thrown")
             throw e
           case e: Exception =>
@@ -101,4 +110,13 @@ class BasicEvaluationCtx(override val evaluationId: Long, request: RequestCtx,
       }
     }
   }
+
+  /**
+   * Return the implementation of the requested combination algorithm.
+   */
+  def getCombinationAlgorithmImplementation(algo: CombinationAlgorithm): CombinationAlgorithmImplementation = algo match {
+      case PermitOverrides => algos.PermitOverrides
+      case DenyOverrides => algos.DenyOverrides
+      case FirstApplicable => algos.FirstApplicable
+    }
 }
